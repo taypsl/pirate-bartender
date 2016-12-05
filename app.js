9c47040d0a4d7458580afee56d7e99e505a4f5ba
Taylor Parsell <tayparsell@gmail.com>
2016-12-05 09:18:02 -0800

    store user preferences

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
+$(document).ready(function(){
+	//constructor functions
+	var state = {
+		currentQuestion: 0,
+		userPreferences: {}
+	}
 
-var ingredients = {
-	strong: ['glug of rum','slug of whisky', 'splash of gin'],
-	salty: ['olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
-	bitter: ['shake of bitters', 'splash of tonic', 'twist of lemon peel']
-	sweet: ['sugar cube', 'spoonful of honey', 'splash of cola']
-	fruity: ['slice of orange', 'dash of cassis', 'cherry on top']
-}
+	function Question(type, text) {
+		this.type = type; 
+		this.text = text;
+	}
 
-function Question(type, text) {
-	this.type = type; 
-	this.text = text;
-}
-
-var questions = [
+	var questions = [
 	new Question('strong', 'Do ye like yer drinks strong?'),
 	new Question('salty', 'Do ye like it with a salty tang?'),
 	new Question('bitter', 'Are ye a lubber who likes it bitter?'),
 	new Question('sweet', 'Would ye like a bit of sweetness with yer poison?'),
 	new Question('fruity', 'Are ye one for a fruity finish?'),
-]
+	]
 
-function Ingredient(type, ingredient) = {
-	this.type = type;
-	this.ingredient = ingredient;
-}
+	function Ingredient(type, ingredient){
+		this.type = type;
+		this.ingredient = ingredient;
+	}
 
-var ingredients = [
+	var ingredients = [
 	new Ingredient('strong', 'glug of rum'),
 	new Ingredient('strong', 'slug of whisky'),
 	new Ingredient('salty', 'olive on a stick'),
@@ -36,9 +34,56 @@ var ingredients = [
 	new Ingredient('sweet', 'spoonful of honey'),
 	new Ingredient('fruity', 'slice of orange'),
 	new Ingredient('fruity', 'dash of cassis'),	
-]
+	]
 
+	//render 
+	function startQuestions(state){
+		$('h2').text(questions[state.currentQuestion].text);
+	}
 
+	function nextPage(state){
+		state.currentQuestion += 1;
+		$('h2').text(questions[state.currentQuestion].text);
+	}
 
+	function Bartender(){
+		this.createDrink = function() {
+			var userChoice;
+			this.createDrink = [];
+			for (userChoice in state.userPreferences) {
+				if(state.userPreferences[userChoice]) {
+					this.createDrink.push(ingredients.randomChoice(userChoice).ingredient)
+				}
+			};
+		}
+	}
 
+	function randomChoice(){
+	   	var newNumber = Math.floor((Math.random() * 3) + 0);
+	   	return newNumber
+	}
+
+//listeners
+startQuestions(state);
+$('.answer-no').on('click', function(e){
+	if (state.currentQuestion < questions.length) {
+		nextPage(state);
+	}
+	else {
+		Bartender.createDrink();
+	}})
+
+$('.answer-yes').on('click', function(e){
+	e.preventDefault();
+
+	if (state.currentQuestion < questions.length) {
+		state.userPreferences += questions[state.currentQuestion].type; 
+		nextPage(state);
+	}
+	else {
+		Bartender.createDrink();
+	}
+})
+
+});
 
